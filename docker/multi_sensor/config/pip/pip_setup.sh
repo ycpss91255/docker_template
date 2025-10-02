1aff5322641012756fb02e2f436a6ff5f44667ae
cyc <ycpss91255@gmail.com>
2025-10-02 20:29:38 +0800

    adjust: pip and shell config

--- docker/multi_sensor/config/pip/pip_setup.sh
+++ docker/multi_sensor/config/pip/pip_setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+set -eux -o pipefail
+
 # python3 -m pip install --upgrade --force-reinstall pip \
 # && pip3 install -r ./pip/requirements.txt
 file_dir=$(dirname "$(readlink -f "${0}")")
 
-pip install --upgrade --force-reinstall pip \
-&& pip install -r "${file_dir}"/requirements.txt
+pip install --upgrade --force-reinstall pip
+pip install -r "${file_dir}"/requirements.txt
--- docker/multi_sensor/config/shell/terminator/terminator_setup.sh
+++ docker/multi_sensor/config/shell/terminator/terminator_setup.sh
@@ -1,13 +1,33 @@
 #!/usr/bin/env bash
 
+set -eux -o pipefail
+
+function check_deps() {
+    local -r _deps=("terminator")
+    local _dep=""
+
+    for _dep in "${_deps[@]}"; do
+        if ! command -v "${_dep}" &> /dev/null; then
+            printf "Error: %s is not installed." "${_dep}" >&2
+            return 1
+        fi
+    done
+    return 0
+}
+
 function main() {
     local -r _username="${1:-"$USER"}"
     local -r _usergroup="${2:-"$(id -gn "${_username}")"}"
     local -r _script_path="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
-    mkdir -p /home/"${_username}"/.config/terminator
-    cp -r "${_script_path}"/config /home/"${_username}"/.config/terminator/config
-    chown -R "${_username}":"${_usergroup}" /home/"${_username}"/.config/terminator
+    mkdir -p -- "${HOME}/.config/terminator"
+    cp -r "${_script_path}/config" "${HOME}/.config/terminator/config"
+    chown -R "${_username}":"${_usergroup}" "${HOME}/.config/terminator"
 }
 
-main
+if check_deps; then
+    main
+else
+    printf "Error: Missing dependencies." >&2
+    exit 1
+fi
